bf8b1fa28fc8979c3ed49c54cb111e50a46513f7
Gleb Dolgich <glebd@me.com>
2022-04-13 14:40:45 +0100

    Tweak rsync command

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ USER=dh_gbi6e7
 HOST=giles.dreamhost.com
 DIR=/home/dh_gbi6e7/cppclub.uk/
 
-hugo && rsync -avz --exclude '.well-known' --exclude '.DS_Store' --delete public/ ${USER}@${HOST}:${DIR}
+hugo && rsync -avzhm --exclude-from=rsync-exclude.lst --delete public/ ${USER}@${HOST}:${DIR}
