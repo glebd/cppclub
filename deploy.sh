9d48d5453fbc8564eceb7e22f1f5ddac84098d42
Gleb Dolgich <glebd@me.com>
2022-04-20 00:32:31 +0100

    deploy.sh: Add rst2html security policy

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ USER=dh_gbi6e7
 HOST=giles.dreamhost.com
 DIR=/home/dh_gbi6e7/cppclub.uk/
 
-hugo && rsync -avzhm --exclude-from=rsync-exclude.lst --delete public/ ${USER}@${HOST}:${DIR}
+HUGO_SECURITY_EXEC_ALLOW=rst2html.py hugo && rsync -avzhm --exclude-from=rsync-exclude.lst --delete public/ ${USER}@${HOST}:${DIR}
