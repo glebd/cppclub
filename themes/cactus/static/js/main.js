8f1d5a5654e37be7ca16005a5966553d0033216d
Gleb Dolgich <glebd@me.com>
2023-07-14 00:11:05 +0100

    Fix AsciiDoc admonitions

--- themes/cactus/static/js/main.js
+++ themes/cactus/static/js/main.js
@@ -10,6 +10,36 @@ if (!!$.prototype.justifiedGallery) {
   $(".article-gallery").justifiedGallery(options);
 }
 
+
+// replace the default admonitions block with one that looks like the antora output to apply similar styling via adoc.css
+window.addEventListener('load', function () {
+  const admonitions = document.getElementsByClassName('admonition-block')
+  for (let i = admonitions.length - 1; i >= 0; i--) {
+    const elm = admonitions[i]
+    const type = elm.classList[1]
+    const text = elm.getElementsByTagName('p')[0].innerHTML
+    const parent = elm.parentNode
+    const tempDiv = document.createElement('div')
+    tempDiv.innerHTML = `<div class="admonitionblock ${type}">
+    <table>
+      <tbody>
+        <tr>
+          <td class="icon">
+            <i class="fa icon-${type}" title="${type}"></i>
+          </td>
+          <td class="content">
+            ${text}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>`
+
+    const input = tempDiv.childNodes[0]
+    parent.replaceChild(input, elm)
+  }
+})
+
 $(document).ready(function() {
 
   /**
@@ -21,7 +51,7 @@ $(document).ready(function() {
 
 
   /**
-   * Controls the different versions of  the menu in blog post articles 
+   * Controls the different versions of  the menu in blog post articles
    * for Desktop, tablet and mobile.
    */
   if ($(".post").length) {
@@ -100,7 +130,7 @@ $(document).ready(function() {
         $("#toc-footer").hide();
         $("#share-footer").hide();
 
-        // show a "navigation" icon when close to the top of the page, 
+        // show a "navigation" icon when close to the top of the page,
         // otherwise show a "scroll to the top" icon
         if (topDistance < 50) {
           $("#actions-footer > #top").hide();
