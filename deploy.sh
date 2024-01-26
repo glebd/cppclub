0a112996a98744b299fad9db17bac84d641d1ea3
Gleb Dolgich <glebd@me.com>
2024-01-26 00:37:25 +0000

    Tweak rsync command (requires brew rsync)

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ USER=dh_gbi6e7
 HOST=giles.dreamhost.com
 DIR=/home/dh_gbi6e7/cppclub.uk/
 
-hugo && rsync -avzhm --exclude-from=rsync-exclude.lst --delete public/ ${USER}@${HOST}:${DIR}
+hugo && rsync --no-inc-recursive -avzhm --exclude-from=rsync-exclude.lst --delete public/ ${USER}@${HOST}:${DIR}
