eff75987b343c26926a7382bc64b2f6aca880111
Gleb Dolgich <glebd@me.com>
2024-08-03 14:20:40 +0100

    Fix deployment script after server migration

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 USER=dh_gbi6e7
-HOST=giles.dreamhost.com
+HOST=cppclub.uk
 DIR=/home/dh_gbi6e7/cppclub.uk/
 
 hugo && rsync --no-inc-recursive -avzhm --exclude-from=rsync-exclude.lst --delete public/ ${USER}@${HOST}:${DIR}
